2b79bdf965026f6dd06e504023592beeae206d5c
Jose Limardo <jose@MacBook-Pro-de-Jose.local>
2023-06-08 16:21:54 -0500

    up

---
+++ assets/Logout-ecdc5845.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-ffd51fc4.js";function r(){return o.jsx("div",{children:o.jsx("div",{className:"loginBody",children:o.jsx("h2",{children:"logout"})})})}export{r as default};
